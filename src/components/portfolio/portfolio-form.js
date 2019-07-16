4f9ac12b5b1c9875e409006c8a74830d62f11bb5
Coleman Shropshire <CS053196@psd.provo.edu>
2019-07-16 11:57:13 -0600

    end of class 7/16

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioForm extends Component {
   constructor(props) {
@@ -16,17 +17,48 @@ export default class PortfolioForm extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.postion);
+
+    return formData;
+  }
   handleChange(event) {
-    console.log("handleChange", event);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://colemanshropshire.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("response:", response);
+      })
+      .catch(error => {
+        console.log("onSubmit error:", error);
+      });
+
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>
         <h1>PortfolioForm</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <input
               type="text"
@@ -48,19 +80,21 @@ export default class PortfolioForm extends Component {
               type="text"
               name="position"
               placeholder="Position"
-              value={this.state.postion}
+              value={this.state.position}
               onChange={this.handleChange}
             />
-            <input
-              type="text"
+            <select
               name="category"
-              placeholder="Category"
               value={this.state.category}
               onChange={this.handleChange}
-            />
+            >
+              <option value="Markup">Markup</option>
+              <option value="Dynamic">Dynamic</option>
+              <option value="Styling">Styling</option>
+            </select>
           </div>
           <div>
-            <input
+            <textarea
               type="text"
               name="description"
               placeholder="Description"
