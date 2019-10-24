056b3a2b794425be316fb915b5871101b678e7b0
Coleman Shropshire <coleshrop@gmail.com>
2019-10-24 09:41:14 -0600

    mod 7 done

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -14,7 +14,10 @@ export default class BlogForm extends Component {
       title: "",
       blog_status: "",
       content: "",
-      featured_image: ""
+      featured_image: "",
+      apiUrl:
+        "https://colemanshropshire.devcamp.space/portfolio/portfolio_blogs",
+      apiAction: "post"
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -31,6 +34,7 @@ export default class BlogForm extends Component {
   }
 
   deleteImage(imageType) {
+    //TODO refactor
     axios
       .delete(
         `https://api.devcamp.space/portfolio/delete-portfolio-blog-image/${this.props.blog.id}?image_type=${imageType}`,
@@ -45,11 +49,15 @@ export default class BlogForm extends Component {
   }
 
   componentWillMount() {
+    //TODO refactor with destructuring
     if (this.props.editMode) {
       this.setState({
         id: this.props.blog.id,
         title: this.props.blog.title,
-        status: this.props.blog.status
+        blog_status: this.props.blog.blog_status,
+        content: this.props.blog.content,
+        apiUrl: `https://colemanshropshire.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+        apiAction: "patch"
       });
     }
   }
@@ -97,16 +105,17 @@ export default class BlogForm extends Component {
   }
 
   handleSubmit(event) {
-    axios
-      .post(
-        "https://colemanshropshire.devcamp.space/portfolio/portfolio_blogs",
-        this.buildForm(),
-        { withCredentials: true }
-      )
+    axios({
+      method: this.state.apiAction,
+      url: this.state.apiUrl,
+      data: this.buildForm(),
+      withCredentials: true
+    })
       .then(response => {
         if (this.state.featured_image) {
           this.featuredImageRef.current.dropzone.removeAllFiles();
         }
+
         this.setState({
           title: "",
           blog_status: "",
@@ -114,7 +123,13 @@ export default class BlogForm extends Component {
           featured_image: ""
         });
 
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
+        if (this.props.editMode) {
+          this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+        } else {
+          this.props.handleSuccessfulFormSubmission(
+            response.data.portfolio_blog
+          );
+        }
       })
       .catch(error => {
         console.log("blog-form handleSubmit error:", error);
