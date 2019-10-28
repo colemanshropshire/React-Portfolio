f2762766a2038cbdb19e4d4b97e6e83ba52f43d8
Coleman Shropshire <coleshrop@gmail.com>
2019-10-28 00:28:48 -0600

    night night

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+import aboutImg from "../../../static/assets/images/about/About_Image.png";
+// Task: image that takes up 50% of the width, 100% of the height, center content on the right side
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="about-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          backgroundImage: `url(${aboutImg})`
+        }}
+      />
+
+      <div className="right-column">
+        <h1>Hi, I am Coleman Shropshire, an aspiring developer.</h1>
+        <p></p>
+      </div>
+    </div>
+  );
 }
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -38,7 +38,9 @@ export default class BlogDetail extends Component {
   }
 
   handleEditClick() {
-    this.setState({ editMode: true });
+    if (this.props.loggedInStatus === "LOGGED_IN") {
+      this.setState({ editMode: true });
+    }
   }
 
   getBlogItem() {
